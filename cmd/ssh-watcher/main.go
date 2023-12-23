7b390148144ea23835e2fd67e63acd7d7bbc99c5
Matt Gottlieb <m.gottlieb@striveworks.us>
2023-12-23 13:38:46 -0600

    clean up config w/ envconfig (#4)
    * clean up config w/ envconfig

--- cmd/ssh-watcher/main.go
+++ cmd/ssh-watcher/main.go
@@ -2,117 +2,54 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 	"time"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/mgla96/ssh-watcher/internal/notifier"
 	"github.com/mgla96/ssh-watcher/internal/watcher"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-const (
-	defaultSlackChannel                           = "#ssh-alerts"
-	defaultSlackUsername                          = "poe-ssh-bot"
-	defaultSlackIcon                              = ":ghost:"
-	defaultLogFileLocation                        = "/var/log/auth.log"
-	defaultWatchAcceptedLogins                    = true
-	defaultWatchFailedLogins                      = false
-	defaultWatchFailedLoginInvalidUsername        = false
-	defaultWatchSleepIntervalSeconds              = 2
-	envKeyHostUrl                                 = "HOST_URL"
-	envKeySlackWebhookUrl                         = "SLACK_WEBHOOK_URL"
-	envKeySlackChannel                            = "SLACK_CHANNEL"
-	envKeySlackUsername                           = "SLACK_USERNAME"
-	envKeySlackIcon                               = "SLACK_ICON"
-	envKeyWatchLogfile                            = "WATCH_LOGFILE"
-	envKeyWatchSettingsAcceptedLogin              = "WATCH_SETTINGS_ACCEPTED_LOGIN"
-	envKeyWatchSettingsFailedLogin                = "WATCH_SETTINGS_FAILED_LOGIN"
-	envKeyWatchSettingsFailedLoginInvalidUsername = "WATCH_SETTINGS_FAILED_LOGIN_INVALID_USERNAME"
-	envKeyWatchInterval                           = "WATCH_INTERVAL"
-)
+type SlackConfig struct {
+	WebhookUrl string `envconfig:"SLACK_WEBHOOK_URL"`
+	Channel    string `envconfig:"SLACK_CHANNEL" default:"#ssh-alerts"`
+	Username   string `envconfig:"SLACK_USERNAME" defaul:"poe-ssh-bot"`
+	Icon       string `envconfig:"SLACK_ICON" default:":ghost:"`
+}
 
 type Config struct {
-	HostUrl                         string
-	WebhookUrl                      string
-	SlackChannel                    string
-	SlackUsername                   string
-	SlackIcon                       string
-	LogFileLocation                 string
-	WatchAcceptedLogin              bool
-	WatchFailedLogin                bool
-	WatchFailedLoginInvalidUsername bool
-	WatchSleepIntervalSeconds       int
+	HostMachineName                 string       `envconfig:"HOST_MACHINE_NAME" required:"true"`
+	Slack                           *SlackConfig `envconfig:"SLACK"`
+	LogFileLocation                 string       `envconfig:"WATCH_LOGFILE" default:"/var/log/auth.log"`
+	WatchAcceptedLogin              bool         `envconfig:"WATCH_SETTINGS_ACCEPTED_LOGIN" default:"true"`
+	WatchFailedLogin                bool         `envconfig:"WATCH_SETTINGS_FAILED_LOGIN" default:"true"`
+	WatchFailedLoginInvalidUsername bool         `envconfig:"WATCH_SETTINGS_FAILED_LOGIN_INVALID_USERNAME" default:"false"`
+	WatchSleepIntervalSeconds       int          `envconfig:"WATCH_SETTINGS_SLEEP_INTERVAL_SECONDS" default:"2"`
 }
 
-func loadConfig() Config {
+func loadConfig() (*Config, error) {
 	c := Config{}
-
-	c.HostUrl = getEnvOrPanic(envKeyHostUrl)
-	c.WebhookUrl = getEnvOrPanic(envKeySlackWebhookUrl)
-
-	c.SlackChannel = getEnvOrDefault(envKeySlackChannel, defaultSlackChannel)
-	c.SlackUsername = getEnvOrDefault(envKeySlackUsername, defaultSlackUsername)
-	c.SlackIcon = getEnvOrDefault(envKeySlackIcon, defaultSlackIcon)
-	c.LogFileLocation = getEnvOrDefault(envKeyWatchLogfile, defaultLogFileLocation)
-
-	c.WatchAcceptedLogin = parseBoolEnv(envKeyWatchSettingsAcceptedLogin, defaultWatchAcceptedLogins)
-	c.WatchFailedLogin = parseBoolEnv(envKeyWatchSettingsFailedLogin, defaultWatchFailedLogins)
-	c.WatchFailedLoginInvalidUsername = parseBoolEnv(envKeyWatchSettingsFailedLoginInvalidUsername, defaultWatchFailedLoginInvalidUsername)
-
-	c.WatchSleepIntervalSeconds = parseIntEnv(envKeyWatchInterval, defaultWatchSleepIntervalSeconds)
-
-	return c
-}
-
-func getEnvOrPanic(key string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		panic(fmt.Sprintf("%s not set", key))
-	}
-	return value
-}
-
-func getEnvOrDefault(key, defaultValue string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		log.Warn().Msgf("%s not set, defaulting to %s", key, defaultValue)
-		return defaultValue
-	}
-	return value
-}
-
-func parseBoolEnv(key string, defaultValue bool) bool {
-	valueStr := os.Getenv(key)
-	value, err := strconv.ParseBool(valueStr)
+	err := envconfig.Process("", &c)
 	if err != nil {
-		log.Warn().Msgf("%s not parsable, defaulting to %t", key, defaultValue)
-		return defaultValue
+		return nil, err
 	}
-	return value
-}
-
-func parseIntEnv(key string, defaultValue int) int {
-	valueStr := os.Getenv(key)
-	value, err := strconv.ParseInt(valueStr, 10, 0)
-	if err != nil {
-		log.Warn().Msgf("%s not parsable, defaulting to %d", key, defaultValue)
-		return defaultValue
-	}
-	return int(value)
+	return &c, nil
 }
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	log.Info().Msg("Starting watcher")
-	config := loadConfig()
+	config, err := loadConfig()
+	if err != nil {
+		panic(err)
+	}
 
-	notifier := notifier.NewSlackNotifier(config.WebhookUrl, config.SlackChannel, config.SlackUsername, config.SlackIcon)
+	notifier := notifier.NewSlackNotifier(config.Slack.WebhookUrl, config.Slack.Channel, config.Slack.Username, config.Slack.Icon)
 	watcher := watcher.NewLogWatcher(
 		config.LogFileLocation,
 		notifier,
-		config.HostUrl,
+		config.HostMachineName,
 		watcher.WatchSettings{
 			WatchAcceptedLogins:             config.WatchAcceptedLogin,
 			WatchFailedLogins:               config.WatchFailedLogin,
@@ -121,6 +58,6 @@ func main() {
 		},
 	)
 
-	log.Info().Msg(fmt.Sprintf("starting watcher, webhook url: %s, logfile: %s", config.WebhookUrl, config.LogFileLocation))
+	log.Info().Msg(fmt.Sprintf("starting watcher, webhook url: %s, logfile: %s", config.Slack.WebhookUrl, config.LogFileLocation))
 	watcher.Watch()
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/mgla96/ssh-watcher
 
 go 1.21.4
 
-require github.com/rs/zerolog v1.31.0
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/rs/zerolog v1.31.0
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
