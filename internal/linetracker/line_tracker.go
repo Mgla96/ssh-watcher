230a42bcf19d37f83c481307c799fe905378384e
Mgla96 <mattgottlieb96@gmail.com>
2024-01-09 05:06:56 +0000

    clean up wrapped errors and create state file directory if not exists

--- internal/linetracker/line_tracker.go
+++ internal/linetracker/line_tracker.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/rs/zerolog/log"
@@ -15,6 +16,11 @@ func NewFileProcessedLineTracker(stateFilePath string) FileProcessedLineTracker 
 	}
 }
 
+func handleError(message string, err error) error {
+	log.Error().Err(err).Msg(message)
+	return fmt.Errorf("%v: %w", message, err)
+}
+
 type FileProcessedLineTracker struct {
 	StateFilePath string
 }
@@ -22,16 +28,14 @@ type FileProcessedLineTracker struct {
 // GetLastProcessedLine reads the statefile and extracts the last processed line number
 // in the ssh log file.
 func (f FileProcessedLineTracker) GetLastProcessedLine() (int, error) {
-	if _, err := os.Stat(f.StateFilePath); os.IsNotExist(err) {
-		message := "state file does not exist"
-		return 0, fmt.Errorf("%v: %w", message, err)
+	dir := filepath.Dir(f.StateFilePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return 0, handleError("failed to create directory for state file", err)
 	}
 
-	state, err := os.Open(f.StateFilePath)
+	state, err := os.OpenFile(f.StateFilePath, os.O_CREATE|os.O_RDONLY, 0644)
 	if err != nil {
-		message := "failed opening state file"
-		log.Error().Err(err).Msg(message)
-		return 0, fmt.Errorf("%v: %w", message, err)
+		return 0, handleError("failed opening or creating state file", err)
 	}
 	defer state.Close()
 
@@ -40,16 +44,12 @@ func (f FileProcessedLineTracker) GetLastProcessedLine() (int, error) {
 	for scanner.Scan() {
 		currentLine, err = strconv.Atoi(scanner.Text())
 		if err != nil {
-			message := "failed converting state file line to int"
-			log.Error().Err(err).Msg(message)
-			return 0, fmt.Errorf("%v: %w", message, err)
+			return 0, handleError("failed converting state file line to int", err)
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		message := "error while reading state file"
-		log.Error().Err(err).Msg("error while reading state file")
-		return 0, fmt.Errorf("%v: %w", message, err)
+		return 0, fmt.Errorf("%v: %w", "error while reading state file", err)
 	}
 
 	return currentLine, nil
@@ -58,22 +58,16 @@ func (f FileProcessedLineTracker) GetLastProcessedLine() (int, error) {
 func (f FileProcessedLineTracker) UpdateLastProcessedLine(lineNumber int) error {
 	state, err := os.Create(f.StateFilePath)
 	if err != nil {
-		message := "failed to create or truncate state file"
-		log.Error().Err(err).Msg(message)
-		return fmt.Errorf("%v: %w", message, err)
+		return handleError("failed to create or truncate state file", err)
 	}
 	defer state.Close()
 
 	if _, err := state.WriteString(fmt.Sprintf("%d", lineNumber)); err != nil {
-		message := "failed to write to state file"
-		log.Error().Err(err).Msg(message)
-		return fmt.Errorf("%v: %w", message, err)
+		return handleError("failed to write to state file", err)
 	}
 
 	if err := state.Sync(); err != nil {
-		message := "failed to syc state file"
-		log.Error().Err(err).Msg(message)
-		return fmt.Errorf("%v: %w", message, err)
+		return handleError("failed to syc state file", err)
 	}
 
 	return nil
