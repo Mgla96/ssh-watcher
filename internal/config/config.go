a8c4c7255e311fa22476038d5f8ccbf29d16060c
Matthew Gottlieb <m.gottlieb@striveworks.us>
2024-03-30 13:06:46 -0500

    clean up docstrings

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,24 +17,24 @@ type SlackConfig struct {
 }
 
 type Config struct {
-	HostMachineName string       `envconfig:"HOST_MACHINE_NAME" required:"true"`
-	Slack           *SlackConfig `envconfig:"SLACK"`
-	LogFileLocation string       `envconfig:"WATCH_LOGFILE" default:"/var/log/auth.log"`
-	// WatchAcceptedLogin              bool         `envconfig:"WATCH_SETTINGS_ACCEPTED_LOGIN" default:"true"`
-	// WatchFailedLogin                bool         `envconfig:"WATCH_SETTINGS_FAILED_LOGIN" default:"true"`
-	// WatchFailedLoginInvalidUsername bool         `envconfig:"WATCH_SETTINGS_FAILED_LOGIN_INVALID_USERNAME" default:"false"`
-	// WatchSleepIntervalSeconds       int          `envconfig:"WATCH_SETTINGS_SLEEP_INTERVAL_SECONDS" default:"2"`
+	HostMachineName string        `envconfig:"HOST_MACHINE_NAME" required:"true"`
+	Slack           *SlackConfig  `envconfig:"SLACK"`
+	LogFileLocation string        `envconfig:"WATCH_LOGFILE" default:"/var/log/auth.log"`
+	WatchSettings   WatchSettings `split_words:"true"`
 	// StateFilePath is location of file that keeps track of the last processed line
 	// by ssh watcher so restarts of the service do not reprocess all ssh history.
-	WatchSettings WatchSettings `split_words:"true"`
-	StateFilePath string        `envconfig:"STATE_FILE_PATH" default:"/var/lib/ssh-watcher/authlog-state"`
+	StateFilePath string `envconfig:"STATE_FILE_PATH" default:"/var/lib/ssh-watcher/authlog-state"`
 }
 
 type WatchSettings struct {
-	AcceptedLogins             bool `default:"true" split_words:"true"`
-	FailedLogins               bool `default:"false" split_words:"true"`
+	// AcceptedLogins is a flag to watch for successful logins
+	AcceptedLogins bool `default:"true" split_words:"true"`
+	// FailedLogins is a flag to watch for failed logins
+	FailedLogins bool `default:"false" split_words:"true"`
+	// FailedLoginInvalidUsername is a flag to watch for failed logins with invalid username
 	FailedLoginInvalidUsername bool `default:"true" split_words:"true"`
-	SleepInterval              int  `default:"2" split_words:"true"`
+	// SleepInterval is the interval in seconds to sleep between log file reads
+	SleepInterval int `default:"2" split_words:"true"`
 }
 
 func New() (*Config, error) {
